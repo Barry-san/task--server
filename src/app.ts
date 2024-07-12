cc91680d3b1fc560f560babdcd784e0ac1ca6fdb
Barry-san <oyeyemimubarak6@gmail.com>
2024-07-12 21:44:50 +0100

    feat: added project route to app

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express, { ErrorRequestHandler } from "express";
 import httpstatus from "http-status-codes";
 import { rootHandler } from "./root/root.handler";
 import { userRouter } from "./user/user.handler";
+import { projectRoute } from "./projects/project.handler";
 
 export const app = express();
 app.use(express.json());
 app.get("/", rootHandler);
 app.use("/users", userRouter);
+app.use("/projects", projectRoute);
 const errorHanlder: ErrorRequestHandler = (err, req, res, next) => {
   if (err.isOperational) {
     res.status(err.statusCode).json({ isSuccess: false, message: err.message });
